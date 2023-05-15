b706575c283d66a7b67fe0f87490dc962f97f83a
Rodolfo Ruiz <rodolfo@hertzian.io>
2023-05-15 13:52:32 +0300

    A simple window with dioxus desktop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    // launch the dioxus app in a webview
+    dioxus_desktop::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "A Simple Window!"
+        }
+    })
 }
